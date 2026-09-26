04940d59aab521abe9ba42329e17c7f2e57cf82e
Tharanika <Tharanika.balasingam@hva.nl>
2026-09-26 18:10:13 +0200

    javascript

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-open");
+}
+
+console.log("JavaScript werkt!");
